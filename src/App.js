c0241732e6dd13c5a3441c701efe266510fcae46
Davi <davi.guimaraesleite@gmail.com>
2020-02-14 15:34:38 -0300

    refactoring.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
+import { Provider } from "react-redux";
 import Settings from "./components/Settings";
 import Timer from "./components/Timer";
-import { Provider } from "react-redux";
 
 import store from "./store";
 
